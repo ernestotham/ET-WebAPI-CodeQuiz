e1f99c62fafb0c72acab7fbfec645e920aa7fb9b
Ernesto Tham <ernestotha@gmail.com>
2021-11-20 02:10:13 -0500

    added function to display answer for 680 milliseconds

--- Assets/js/helper.js
+++ Assets/js/helper.js
@@ -22,7 +22,7 @@ function startQuiz() {
 function validateQuestion(Qnum, respond) {
 
     return (correctAns[Qnum] == respond) ? "Correct" : "Incorrect"
-    
+
 }
 
 
@@ -34,23 +34,36 @@ function startNextQuestion() {
         questionAns = 0;
         for (var questionAns = 0; questionAns < Qbuttons.length; questionAns++) {
 
+
+            answerContainer.style.display = 'block';
             Qbuttons[questionAns].textContent = answers[Qnumber][questionAns]
             console.log(Qbuttons[questionAns])
             console.log(answers[Qnumber][questionAns])
+            displayAnswer()
 
         }
 
-    }//end if
-
+    } //end if
     else {
         isLastQuestion = true;
         QuestionContainer.style.display = 'none';
         AllDonecontainer.style.display = 'block';
+        answerContainer.style.display = 'block';
+        displayAnswer()
 
-    }//end else
+    } //end else
 
 
 }
+
+function displayAnswer() {
+
+    setTimeout(() => {
+        answerContainer.style.display = 'none';
+    }, 680);
+
+}
+
 
 function QuizCompleted() {
 
@@ -59,7 +72,7 @@ function QuizCompleted() {
 
 function startTimer() {
     // Sets timer
-    timer = setInterval(function () {
+    timer = setInterval(function() {
         timerCount--;
         timerCountEl.textContent = timerCount;
         console.log("timer: " + timerCount)
@@ -72,47 +85,47 @@ function startTimer() {
                 timerCount = timerCount - 10;
                 timerCountEl.textContent = timerCount;
                 Trackanswer = ""
-            }//end inner if
+            } //end inner if
 
-        }//end if
+        } //end if
 
 
         if (isLastQuestion || timerCount === 0) {
             console.log("is this the last questions: " + isLastQuestion)
             console.log("Current timer: " + timerCount)
-            // Clears interval
+                // Clears interval
             clearInterval(timer);
             scoreEl.textContent = timerCount;
             QuestionContainer.style.display = 'none';
             AllDonecontainer.style.display = 'block';
 
-        }//end if
+        } //end if
     }, 1000);
 
-}//end startTimer Function
+} //end startTimer Function
 
 
 function setScore() {
-        TotalTbl = []
+    TotalTbl = []
     var currentScores = getScores();
-     console.log(currentScores)
-    
-        var intialsTB = document.getElementById("textbox").value
-       
-        console.log(intialsTB)
-        
+    console.log(currentScores)
+
+    var intialsTB = document.getElementById("textbox").value
+
+    console.log(intialsTB)
+
     var NewScore = new Object();
-        NewScore.initials = intialsTB;
-        NewScore.score = timerCount;
-        
-        console.log(NewScore)
-       
+    NewScore.initials = intialsTB;
+    NewScore.score = timerCount;
+
+    console.log(NewScore)
+
     // debugger
     //check local storage results variable is empty : add
     if (currentScores.length === 0) {
         TotalTbl.push(NewScore)
         console.log(JSON.stringify(TotalTbl))
-        
+
         localStorage.setItem("results", JSON.stringify(TotalTbl))
         console.log("inside if - end")
     }
@@ -133,7 +146,3 @@ function getScores() {
     var scoreResults = localStorage.getItem('results') || []
     return scoreResults
 }
-
-
-
-
--- Assets/js/logic.js
+++ Assets/js/logic.js
@@ -3,48 +3,54 @@ var container = document.querySelector(".bodycontent");
 var StartContainer = document.querySelector("#Startcontainer");
 var QuestionContainer = document.querySelector("#Qcontainer");
 var AllDoneContainer = document.querySelector("#AllDonecontainer");
+var answerContainer = document.querySelector("#Qanswer")
 var Trackanswer = ""
-var isLastQuestion = false 
+var isLastQuestion = false
 
 var Questions = ["Inside which HTML element do we put the JavaScript?",
     "What is the correct JavaScript syntax to change the content of the HTML element below?",
     "How do you write \"Hello World\" in an alert box?",
     "How do you create a function in JavaScript?",
-    "How to write an IF statement in JavaScript?"];
-var AnswersQ1 = ["<js>","<script>","<scripting>","<javascript>"];
+    "How to write an IF statement in JavaScript?"
+];
+var AnswersQ1 = ["<js>", "<script>", "<scripting>", "<javascript>"];
 // var CorrectAnsQ1 = "<script>"
 
 var AnswersQ2 = ["#demo.innerHTML = \"Hello World!\";",
-        "document.getElement(\"p\").innerHTML = \"Hello World!\";",
-        "document.getElementById(\"demo\").innerHTML = \"Hello World!\";",
-        "document.getElementByName(\"p\").innerHTML = \"Hello World!"];
+    "document.getElement(\"p\").innerHTML = \"Hello World!\";",
+    "document.getElementById(\"demo\").innerHTML = \"Hello World!\";",
+    "document.getElementByName(\"p\").innerHTML = \"Hello World!"
+];
 // var CorrectAnsQ2 = "document.getElementById(\"demo\").innerHTML = \"Hello World!\";";
 
 var AnswersQ3 = ["alert(\"Hello World\");",
-        "alertBox(\"Hello World\");",
-        "msg(\"Hello World\");",
-        "msgBox(\"Hello World\");"];
+    "alertBox(\"Hello World\");",
+    "msg(\"Hello World\");",
+    "msgBox(\"Hello World\");"
+];
 // var CorrectAnsQ3 = "alertBox(\"Hello World\");";
 
 var AnswersQ4 = ["function myFunction() {}",
-        "function:myFunction {}",
-        "function = myFunction {}",
-        "function=>() {}"];
+    "function:myFunction {}",
+    "function = myFunction {}",
+    "function=>() {}"
+];
 // var CorrectAnsQ4 = "function myFunction() {}";
 
 var AnswersQ5 = ["if i == 5 then",
-        "if i = 5",
-        "if(i == 5)",
-        "if i =5 then"];
+    "if i = 5",
+    "if(i == 5)",
+    "if i =5 then"
+];
 // var CorrectAnsQ5 = "if(i == 5)";
 
-var answers = [AnswersQ1,AnswersQ2,AnswersQ3,AnswersQ4, AnswersQ5]
+var answers = [AnswersQ1, AnswersQ2, AnswersQ3, AnswersQ4, AnswersQ5]
 var correctAns = ["<script>",
     "document.getElementById(\"demo\").innerHTML = \"Hello World!\";",
     "alertBox(\"Hello World\");",
     "function myFunction() {}",
     "if(i == 5)"
-    ]
+]
 
 
 
@@ -52,46 +58,42 @@ var correctAns = ["<script>",
 container.getAttribute("")
 QuestionContainer.style.display = 'none';
 AllDoneContainer.style.display = 'none';
+answerContainer.style.display = 'none';
 var stateEvent = Event.target;
 
-var Buttons = document.addEventListener("click", function(event){
+var Buttons = document.addEventListener("click", function(event) {
     var targetBTN = event.target
     console.log(event)
     console.log(targetBTN)
     console.log(event.target.id)
     console.log(event.target.className)
 
-    if(event.target.id === "startBtn"){
+    if (event.target.id === "startBtn") {
         startQuiz();
 
 
     }
 
-    if(event.target.className === "Qbtn"){
-        console.log(Qnumber +' '+ event.target.outerText)
+    if (event.target.className === "Qbtn") {
+        console.log(Qnumber + ' ' + event.target.outerText)
         var answer4CQ = validateQuestion(Qnumber, event.target.outerText)
         console.log(answer4CQ)
         document.getElementById("answer").textContent = answer4CQ
         Qnumber++;
-        Trackanswer =  answer4CQ;
+        Trackanswer = answer4CQ;
         startNextQuestion();
 
     }
-    
-    if(event.target.id === "submit"){
+
+    if (event.target.id === "submit") {
         event.preventDefault();
         setScore()
         window.location.replace("../highscores.html");
-        
+
 
     }
 
-  
+
 
 
 });
-
-
-
-
-
